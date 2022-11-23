ebe6b76310ac93fd82ee9fac56b0af2f09460160
Md. Mesbahose Salekeen <57279626+alvi1995@users.noreply.github.com>
2022-11-24 01:02:10 +0600

    Add files via upload

---
+++ project/operation/include/operation.h
@@ -0,0 +1,8 @@
+#include "fraction.h"
+
+class operation
+{
+    public:
+        fraction add(fraction a, fraction b);
+        ~operation();
+};
---
+++ project/operation/src/operation.cpp
@@ -0,0 +1,8 @@
+#include "operation.h"
+
+fraction operation::add(fraction a, fraction b)
+{
+    return a+b;
+}
+
+operation::~operation() { }
